3f07a3106a6a57f0621809ef6c11f2857e9f8853
Koki Hashimoto <gohannnotomo@gmail.com>
2020-08-10 16:48:45 +0900

    適当な位置情報をファイルに書き出すクラスを作成した。 毎秒ランダムに更新する

---
+++ move.rb
@@ -0,0 +1,33 @@
+require 'FileUtils'
+require 'json'
+
+START=[31.9122178,131.4075771] # [latitude,longitude]
+DATA_DIR = "./data"
+
+class Pos
+  def initialize(id, start = nil)
+    @id = id
+    @start = start || START
+    @file = File.open("#{DATA_DIR}/#{id}.json","w")
+  end
+
+  def move
+    @x ||=  @start[0]
+    @y ||=  @start[1]
+    @x += rand(-1.0..1.0)
+    @y += rand(-1.0..1.0)
+    @file.rewind 
+    json = JSON.dump(@id => {latitude: @x, longitude: @y})
+    @file.write(json)
+    @file.flush
+    @file.truncate(@file.pos)
+  end
+end
+
+FileUtils.mkdir_p(DATA_DIR)
+pos_a = Pos.new("a")
+
+while(true) do
+  pos_a.move
+  sleep(1)
+end
