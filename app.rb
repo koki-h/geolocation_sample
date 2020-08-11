bed4057fe88262adbc517eb726eda1b80c8898cd
Koki Hashimoto <gohannnotomo@gmail.com>
2020-08-11 13:04:57 +0900

    特定領域内に存在するマーカのみを返すAPIを作成した

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'json'
 
+DATA_DIR = 'public/data'
+SQUARE_WIDTH_OF_LONGITUDES = 0.1 # マーカを表示する、画面中心からの縦横の距離(度)
 
 set :bind, '0.0.0.0'
 
@@ -9,6 +12,27 @@ get '/health_check' do
 end
 
 get '/markers' do
-  p params['center']
-  puts "bbb"
+  lat, long =  params['center'].split(",").map(&:to_f)
+  load_markers(lat, long)
+end
+
+def load_markers(lat, long)
+  marker_area = {
+    latitude: (lat - SQUARE_WIDTH_OF_LONGITUDES..lat + SQUARE_WIDTH_OF_LONGITUDES),
+    longitude: (long - SQUARE_WIDTH_OF_LONGITUDES)..(long + SQUARE_WIDTH_OF_LONGITUDES),
+  }
+  p marker_area
+  markers = Dir.glob("#{DATA_DIR}/*.json").map do |f|
+    json = open(f).read
+    data = JSON.parse(json)
+    pos = data.values.first
+    p pos
+    if (marker_area[:latitude].include?(pos["latitude"])  &&
+        marker_area[:longitude].include?(pos["longitude"]))
+      data
+    end
+  end
+  markers.compact!
+  p markers
+  JSON.dump(markers)
 end
