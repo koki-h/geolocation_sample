8599a6f78f05d1ff6fbceb2de4a9651af6be3f43
Koki Hashimoto <gohannnotomo@gmail.com>
2020-08-11 11:36:51 +0900

    アプリをオートリロードするようにした

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
+
 
 set :bind, '0.0.0.0'
 
 get '/health_check' do
   'health check'
+end
+
+get '/markers' do
+  p params['center']
+  puts "bbb"
 end
