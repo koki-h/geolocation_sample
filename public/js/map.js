8c4b965c8660c348db03c9b378bd670e2e81228f
Koki Hashimoto <gohannnotomo@gmail.com>
2020-08-18 12:52:39 +0900

    js の変数宣言をすべてletにした

--- public/js/map.js
+++ public/js/map.js
@@ -5,26 +5,26 @@ function Map(map, center){
       .then(data => callback(data));
   }
 
-  var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  let tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   });
 
-  var markers = [];
-  var last_markers_of_ids = [];
+  let markers = [];
+  let last_markers_of_ids = [];
   map.addLayer(tiles);
 
   setInterval(function(){
     loadMarkerPos(data => {
-      var count = Object.keys(data).length
+      let count = Object.keys(data).length
       document.getElementById("marker_count").textContent = "count of markers: " + count;
       Object.keys(data).forEach(id => {
-        var pos = data[id];
+        let pos = data[id];
         console.log("----------")
         console.log(id);
         console.log(pos);
-        var x = pos["latitude"];
-        var y = pos["longitude"];
+        let x = pos["latitude"];
+        let y = pos["longitude"];
         if (markers[id] == undefined) {
           markers[id] = L.Marker.movingMarker([[x, y]], [], {});
           markers[id].bindPopup(id).addTo(map);
